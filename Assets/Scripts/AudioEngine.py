7ec43c45d32722160237a6f4fff9c3c6c43f2f9a
HD-Nyx <raycastoflight@gmail.com>
2026-04-24 09:50:22 +1200

    added vb cable

--- Assets/Scripts/AudioEngine.py
+++ Assets/Scripts/AudioEngine.py
@@ -4,15 +4,23 @@ import soundfile as SoundFile
 from threading import Thread
 import time
 
+# for testing
 Sound, Sample = SoundFile.read(r"C:\.Coding\GitHub\Pyboard\RAHHH.wav")
+print(f"Sample rate: {Sample}")
+print(f"Shape: {Sound.shape}") 
+print(f"dtype: {Sound.dtype}")  
+
+# Gets CABLE Input from list
+CableIndex = next((i for i, D in enumerate(SoundDevice.query_devices()) if "CABLE Input" in D["name"]), None)
 
 def PlayAudio():
-    SoundDevice.play(Sound, Sample)
+    SoundDevice.play(Sound, 44100, device=CableIndex)
 
-    print("Played audio")
+    print("Played audio\n")
 
     SoundDevice.wait()
 
+# audio thread so that it doesn't block anything else
 AudioThread = Thread(target=PlayAudio)
 AudioThread.name = "AudioThread"
 AudioThread.start()
--- Assets/Scripts/InstallVB_Cable.py
+++ Assets/Scripts/InstallVB_Cable.py
@@ -1,9 +1,16 @@
+import subprocess
 import os
+
+# Apprently winget can't install it, wish it was bash
+# Just have to package the setup EXE and run it
+
+InstallerPath = os.path.abspath(os.path.join(os.path.dirname(__file__), "../Libraries/VBCABLE_Setup_x64.exe"))
 
 def Install():
     print("Installing...")
-    os.system("winget list VBAudio.VBCable")
+    subprocess.run(["powershell","Start-Process", InstallerPath,"-Verb", "RunAs","-Wait"], check=True)
 
 def Uninstall():
     print("Uninstalling...")
     os.system("winget uninstall VBAudio.VBCable")
+
